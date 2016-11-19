7b222ff5a89f4dcffb48684a5c7c5123faec301e
Bertrand Chardon <bertrand.chardon@gmail.com>
2016-11-19 14:02:17 +0100

    WIP
    - The screenshot doesn't capture the right portion of the screen
    - The dimensions and CSS seem to be taken into account though

--- main.js
+++ main.js
@@ -1,44 +1,90 @@
 const electron = require('electron')
+const fs = require('fs')
+const Q = require('q')
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
-const path = require('path')
-const url = require('url')
+const URL = 'https://www.mailchimp.com'
+const dimensions = { width: 2560, height: 1600 }
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+// Should go into a separate module
+const Pinky = {
+  readFile: (path) => {
+    return Q.promise((resolve, reject) => {
+      fs.readFile(path, 'utf-8', (error, contents) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(contents)
+        }
+      })
+    })
+  }
+}
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+const waitFor = (timeout) => {
+  return Q.promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, timeout)
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+class NativeImageEncoder {
+  constructor (nativeImage) {
+    this.nativeImage = nativeImage
+  }
+  encode ({ format, quality = 100 }) {
+    let buffer = Buffer.from([])
+    if (Object.is(format, 'png')) {
+      buffer = this.nativeImage.toPNG()
+    } else {
+      buffer = this.nativeImage.toJPEG(quality)
+    }
+    return buffer
+  }
+}
 
+const takeScreenshot = (contents, { stylesPath, outputFile, dimensions }) => {
+  console.log('Page has finished loading')
+  Pinky.readFile(stylesPath).then(cssDeclarations => {
+    console.log('CSS declarations', cssDeclarations)
+    contents.insertCSS(cssDeclarations)
+    // Take the screenshot and write it to a file
+    const rect = Object.assign(dimensions, { x: 0, y: 0 })
+    // Maybe the injected CSS needs time, the result is not accurate without it
+    waitFor(3000).then(() => {
+      contents.capturePage(rect, (nativeImage) => {
+        // See above, the NativeImageEncoder#encode (format: [jpg|png], [quality: (0..100)]) => Buffer
+        const buffer = new NativeImageEncoder(nativeImage).encode({format: 'jpg'})
+        fs.writeFile(outputFile, buffer)
+        console.log('Wrote file, exiting')
+        app.quit()
+      })
+    })
+    console.log('Waiting for the elements to be ready for the screenshot')
+  }).catch(error => {
+    console.log(error)
+  })
+}
+
+app.on('ready', () => {
+  // Create a new window. Don't show it though, we're only using it to create the screenshot
+  const windowOptions = Object.assign(dimensions, { show: false })
+  let screenshotWindow = new BrowserWindow(windowOptions)
+  // We wait for the window to finish loading
+  const contents = screenshotWindow.webContents
+  contents.on('did-finish-load', () => {
+    takeScreenshot(contents, Object.assign({ dimensions }, {
+      stylesPath: './styles.css',
+      outputFile: `mailchimp-${dimensions.width}-${dimensions.height}.jpg`,
+      outputFormat: 'png'
+    }))
+  })
+  screenshotWindow.loadURL(URL)
+})
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
@@ -47,14 +93,3 @@ app.on('window-all-closed', function () {
     app.quit()
   }
 })
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
